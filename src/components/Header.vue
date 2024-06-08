35c74b11f4161a5e71f64904e08854dd3a6e1d39
paddyTian <q135135x@gmail.com>
2024-06-08 17:53:04 +0800

    feat/Add new components

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <div class="bg"></div>
+        <div class="intro gs-absolute">
+            <h1>
+                I'm Paddy Tian <br />
+                A Front-end developer
+            </h1>
+            <p>Nice to meet you</p>
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.bg {
+    width: 100vw;
+    height: 100vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    /* background-image: url("../images/header-image.jpg"); */
+    background-image: url("@/assets/header-image.jpg");
+}
+.intro {
+    position: absolute;
+    color: white;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="navbar">
+        <div class="nav-container">
+            <div class="logo">
+                <a href="#myHome"> </a>
+            </div>
+            <div class="links-container">
+                <ul class="links show-list-transition">
+                    <li>
+                        <a class="active scroll-link" href="#myHome">Home</a>
+                    </li>
+                    <li><a class="scroll-link" href="#myBio">About</a></li>
+                    <li>
+                        <a class="scroll-link" href="#myPortfolio">Portfolio</a>
+                    </li>
+                    <li><a class="scroll-link" href="#myHabit">Habit</a></li>
+                    <li><a class="scroll-link" href="#myResume">Resume</a></li>
+                    <li><a class="scroll-link" href="#myBlog">Blog</a></li>
+                    <li><a class="scroll-link" href="#myFooter">Contact</a></li>
+                </ul>
+            </div>
+            <div class="toggle-button">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
