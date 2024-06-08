f5e6e146fbcedcb7ea63cce363c50d1e5ca44268
paddyTian <q135135x@gmail.com>
2024-06-08 17:51:36 +0800

    fix/Resolve ts file cant recognizing .vue import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import alias from "@rollup/plugin-alias";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [
+        vue(),
+        alias({
+            entries: [{ find: "@", replacement: "/src" }],
+        }),
+    ],
+});
