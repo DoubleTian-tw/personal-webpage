ea01a899d2d31622e3d0fe35145be47a6b3aa236
Tian <tian@TiandeMacBook-Air.local>
2024-06-30 21:42:44 +0800

    fix/Add typescript-eslint plugin to fix interface issue

--- eslint.config.js
+++ eslint.config.js
@@ -8,4 +8,21 @@ export default [
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     ...pluginVue.configs["flat/essential"],
+    {
+        extends: [
+            "eslint:recommended",
+            "plugin:vue/vue3-recommended",
+            "plugin:@typescript-eslint/recommended",
+        ],
+        parser: "vue-eslint-parser",
+        parserOptions: {
+            parser: "@typescript-eslint/parser",
+            extraFileExtensions: [".vue"],
+            ecmaVersion: 2021,
+        },
+        plugins: ["vue", "@typescript-eslint"],
+        rules: {
+            // Your other ESLint rules
+        },
+    },
 ];
