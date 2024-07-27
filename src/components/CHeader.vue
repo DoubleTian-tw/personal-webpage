cd1f10b4c10d85ff06848861dca845a8f0588c3b
Paddy Tian <q135135x@gmail.com>
2024-07-27 12:05:27 +0800

    feat/Replace bg image and add bg-blend-mode

--- src/components/CHeader.vue
+++ src/components/CHeader.vue
@@ -1,11 +1,11 @@
 <template>
     <header class="relative">
         <div
-            class="w-full h-screen bg-center bg-no-repeat bg-cover bg-fixed bg-[url('@/assets/header-image.jpg')]"></div>
-        <div
-            class="gs-absolute absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+            class="w-full h-screen bg-center bg-no-repeat bg-cover bg-black/75 bg-blend-multiply bg-fixed bg-[url('@/assets/avatar.jpg')]">
+        </div>
+        <div class="gs-absolute absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
             <h1>
-                I'm Paddy Tian <br />
+                I'm Paddy Tian<br />
                 A Front-end developer
             </h1>
             <p class="text-center">Nice to meet you</p>
