731151bec7d43b62bebba6cf256327322aec05d7
Paddy Tian <q135135x@gmail.com>
2024-07-28 17:15:57 +0800

    feat/Adjust constructure, using tailwind and remove unused classes

--- src/components/CGoBack.vue
+++ src/components/CGoBack.vue
@@ -1,21 +1,19 @@
 <template>
-    <a class="scroll-link goBack-link" href="#myHome">
-        <img
-            class="fixed bottom-[10%] right-[5%] w-8 h-auto cursor-pointer"
-            src="@/assets/icon-goBack.png"
-            alt="go back icon"
-            title="Click me to go home page." />
-    </a>
+    <div class="fixed bottom-[10%] right-[5%]">
+        <a class="cursor-pointer" href="#myHome">
+            <img class="w-8 h-auto" src="@/assets/icon-goBack.png" alt="go back icon"
+                title="Click me to go home page." />
+        </a>
+    </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
 img {
-    animation-name: breath;
-    animation-duration: 1s;
-    animation-iteration-count: infinite;
+    @apply animate-[breath_1s_ease_infinite];
 }
+
 @keyframes breath {
     0% {
         transform: translateY(0px);
